fa0aa323842a28a72a4a32a9e4a30c50ed06ba3c
Lucas <lucasdecastrodev@gmail.com>
2024-02-10 17:13:34 -0300

    Learning TypeScript

---
+++ src/type-guards/type-guards.ts
@@ -0,0 +1,6 @@
+export function add(a: unknown, b: unknown): number | string {
+  return typeof a === 'number' && typeof b === 'number' ? a + b : `${a}${b}`;
+}
+
+console.log(add(1, 5));
+console.log(add('a', 'b'));
